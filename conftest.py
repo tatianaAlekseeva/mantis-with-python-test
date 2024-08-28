d7f4dcb64548dad1bcfd701b2e652efed9988c68
Tatiana Alekseeva <tiikeri0578@gmail.com>
2024-08-28 22:11:46 +0200

    Homework 26

--- conftest.py
+++ conftest.py
@@ -22,10 +22,12 @@ def app(request):
     global fixture
     browser = request.config.getoption("--browser")
     web_config = load_config(request.config.getoption("--target"))["web"]
-    web_config_admin = load_config(request.config.getoption("--target"))["webadmin"]
+    # web_config_admin = load_config(request.config.getoption("--target"))["webadmin"]
+    web_config_testuser = load_config(request.config.getoption("--target"))["testuser"]
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=web_config["baseUrl"])
-    fixture.session.ensure_login(username=web_config_admin["username"], password=web_config_admin["password"])
+    # fixture.session.ensure_login(username=web_config_admin["username"], password=web_config_admin["password"])
+    fixture.session.ensure_login(username=web_config_testuser["username"], password=web_config_testuser["password"])
     return fixture
 
 
